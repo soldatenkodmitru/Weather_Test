df7e3252763c50e955b617b94e33583008c2d893
gavrilak <gavrilak89@gmail.com>
2014-11-08 03:38:41 +0200

    Initial Commit

---
+++ Weather_Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weather_Test
+//
+//  Created by Dima on 11/8/14.
+//  Copyright (c) 2014 Dima Soldatenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Weather_TestTests/Weather_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  Weather_TestTests.m
+//  Weather_TestTests
+//
+//  Created by Dima on 11/8/14.
+//  Copyright (c) 2014 Dima Soldatenko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Weather_TestTests : XCTestCase
+
+@end
+
+@implementation Weather_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
